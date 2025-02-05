6950eaa79d3928bfc6aaea543af4b61e76f238a6
김병년 <qud5252@naver.com>
2025-02-05 20:38:17 +0900

    S12P11D106-32 feat: TMBG 로그인 구현 완료 및MBG 로그인 구현 세팅

--- MBG/app/src/main/java/com/ssafy/mbg/MbgApplication.kt
+++ MBG/app/src/main/java/com/ssafy/mbg/MbgApplication.kt
@@ -1,11 +1,16 @@
 package com.ssafy.mbg
 
 import android.app.Application
+import android.util.Log
+import com.ssafy.mbg.util.KeyHashUtil
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MbgApplication : Application() {
+//    private val TAG = "MBG"
     override fun onCreate() {
         super.onCreate()
+//        val key = KeyHashUtil.getKeyHash(this)
+//        Log.d(TAG, key)
     }
 }
