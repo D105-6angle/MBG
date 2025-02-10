e5dd8ded97264090ddd1a69338f8df25e544a597
제갈민 <vlf5231@naver.com>
2025-02-10 14:49:47 +0900

    feat : 공지 알림 전체 조회

--- backend_project/src/main/java/com/ssafy/model/mapper/fcm/TeacherNoticeMapper.java
+++ backend_project/src/main/java/com/ssafy/model/mapper/fcm/TeacherNoticeMapper.java
@@ -1,6 +1,6 @@
 package com.ssafy.model.mapper.fcm;
 
-import com.ssafy.controller.room.RoomResponse;
+import com.ssafy.controller.TeacherNotice.TeacherNoticeDto;
 import com.ssafy.model.entity.TeacherNotice;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Mapper
 public interface TeacherNoticeMapper {
+    List<TeacherNoticeDto> getNoticesByRoomId(Long roomId);
     int insertNotice(TeacherNotice notice);
     TeacherNotice selectNotice(@Param("noticeId") Long noticeId, @Param("roomId") Long roomId);
     List<TeacherNotice> selectNoticesByRoomId(@Param("roomId") Long roomId);
