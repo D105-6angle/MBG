e23c0cff4b0ceb30a6f0511f478005542a291cee
이용재 <lyj3023@naver.com>
2025-02-12 16:15:35 +0900

    feat : 학생어플 방 입장, 조 입장 기능개발
    - 조 선택시 메인 ui에 표시 기능 완료
    - 지역 선택 드롭다운으로 지역 선택 시 userpreference 저장 확인 완료
    
    Resolves: S12P11D106-80

--- MBG/app/src/main/java/com/ssafy/mbg/api/HomeApi.kt
+++ MBG/app/src/main/java/com/ssafy/mbg/api/HomeApi.kt
@@ -1,17 +1,28 @@
 package com.ssafy.mbg.api
 
-import com.ssafy.mbg.data.home.dao.JoinRequest
-import com.ssafy.mbg.data.home.dao.JoinResponse
+import com.ssafy.mbg.data.home.dao.JoinGroupRequest
+import com.ssafy.mbg.data.home.dao.JoinGroupResponse
+import com.ssafy.mbg.data.home.dao.JoinRoomRequest
+import com.ssafy.mbg.data.home.dao.JoinRoomResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface HomeApi {
 
     // 초대코드를 통해 방 입장
     @POST("rooms/join")
     suspend fun joinRooms(
-        @Body request: JoinRequest
-    ) : Response<JoinResponse>
+        @Body request: JoinRoomRequest
+    ) : Response<JoinRoomResponse>
+
+    // 조 선택하여 소속 저장
+    @POST("rooms/{roomId}/groups/select")
+    suspend fun joinGroups(
+        @Path("roomId") roomId: Long,
+        @Body request: JoinGroupRequest
+    ) : Response<JoinGroupResponse>
 
 }
