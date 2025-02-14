7d9ed4d175cf3f3fedc2324c0d5e0a5d12e624c1
김병년 <qud5252@naver.com>
2025-02-14 17:39:59 +0900

    fix: 프로필 정보 못 불러오는 버그 해결 및 도감 버그 해결

--- MBG/app/src/main/java/com/ssafy/mbg/data/book/response/BookDetailResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.ssafy.mbg.data.book.response
-
-data class BookDetailResponse(
-    val cardId : Long,
-    val cardName : String,
-    val imageUrl : String,
-    val collectedAt : String,
-    val codeId : String
-)
