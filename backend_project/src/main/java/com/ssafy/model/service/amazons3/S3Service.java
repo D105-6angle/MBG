12ed2c0a6b4dd604f1be39710ee91693f628e34c
최대규 <choieorb1998@naver.com>
2025-02-14 16:41:37 +0900

    fix: S3 Bucket Upload 메서드 변경

--- backend_project/src/main/java/com/ssafy/model/service/amazons3/S3Service.java
+++ backend_project/src/main/java/com/ssafy/model/service/amazons3/S3Service.java
@@ -1,6 +1,7 @@
 package com.ssafy.model.service.amazons3;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +21,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class S3Service {
     private final S3Client s3Client;
     private final AwsBasicCredentials credentials;
@@ -35,12 +37,51 @@ public class S3Service {
     private String bucket;
 
     // 파일 업로드
+//    public String uploadFile(MultipartFile file, String dirName) throws IOException {
+//        String fileName = createFileName(file.getOriginalFilename());
+//        String fileKey = dirName + "/" + fileName;
+//
+//        PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(bucket).key(fileKey)
+//                .contentType(file.getContentType()).build();
+//
+//        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+//        return fileKey;
+//    }
+
     public String uploadFile(MultipartFile file, String dirName) throws IOException {
+        // 1. 입력값 로깅
+        log.info("=== S3 Upload Debug Info ===");
+        log.info("dirName: {}", dirName);
+        log.info("file is null: {}", file == null);
+
+        if (file != null) {
+            log.info("file.originalFilename: {}", file.getOriginalFilename());
+            log.info("file.contentType: {}", file.getContentType());
+            log.info("file.size: {}", file.getSize());
+        } else {
+            throw new IllegalArgumentException("File must not be null");
+        }
+
+        if (dirName == null) {
+            throw new IllegalArgumentException("Directory name must not be null");
+        }
+
+        // 2. S3 설정값 로깅
+        log.info("bucket name: {}", bucket);
+        log.info("s3Client region: {}", s3Client.serviceClientConfiguration().region());
+
+        // 기존 로직 시작
         String fileName = createFileName(file.getOriginalFilename());
         String fileKey = dirName + "/" + fileName;
 
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(bucket).key(fileKey)
-                .contentType(file.getContentType()).build();
+        // 3. 생성된 키 로깅
+        log.info("generated fileKey: {}", fileKey);
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileKey)
+                .contentType(file.getContentType())
+                .build();
 
         s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
         return fileKey;
