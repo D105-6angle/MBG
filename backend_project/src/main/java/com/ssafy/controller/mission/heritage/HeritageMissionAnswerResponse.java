c2c98ec13c07c2bc1944e1f6ae92a7be01a5a549
최대규 <choieorb1998@naver.com>
2025-02-14 15:19:31 +0900

    fix: 문화제 문제 정답 조회 이미지 URL 수정

--- backend_project/src/main/java/com/ssafy/controller/mission/heritage/HeritageMissionAnswerResponse.java
+++ backend_project/src/main/java/com/ssafy/controller/mission/heritage/HeritageMissionAnswerResponse.java
@@ -2,8 +2,10 @@ package com.ssafy.controller.mission.heritage;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class HeritageMissionAnswerResponse {
     private boolean isCorrect;
