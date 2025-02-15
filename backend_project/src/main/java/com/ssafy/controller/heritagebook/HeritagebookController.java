cb7b55d5a000b6cfa87e29f1277380be763c1c6d
최대규 <choieorb1998@naver.com>
2025-02-15 14:33:58 +0900

    fix: 도감 이미지 URL 수정

--- backend_project/src/main/java/com/ssafy/controller/heritagebook/HeritagebookController.java
+++ backend_project/src/main/java/com/ssafy/controller/heritagebook/HeritagebookController.java
@@ -1,6 +1,7 @@
 package com.ssafy.controller.heritagebook;
 
 
+import com.ssafy.model.service.amazons3.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,18 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.model.service.heritagebook.HeritagebookService;
 
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/users/{userId}/heritagebook")
 @RequiredArgsConstructor
 @Tag(name = "도감", description = "도감 API")
 public class HeritagebookController {
-
     private final HeritagebookService heritagebookService;
+    private final S3Service s3Service;
 
     @Operation(summary = "도감 전체 조회")
     @GetMapping
     public ResponseEntity<HeritagebookResponse.ListResponse> getAllCards(@PathVariable Long userId) {
-        return ResponseEntity.ok(heritagebookService.getAllCards(userId));
+        HeritagebookResponse.ListResponse response = heritagebookService.getAllCards(userId);
+        for (HeritagebookResponse.DetailResponse detail : response.getCards()) {
+            String presignedUrl = s3Service.generatePresignedUrl(detail.getImageUrl());
+            detail.setImageUrl(presignedUrl);
+        }
+        return ResponseEntity.ok(response);
     }
 
 
