6fc7fe49b1a6dedbb09d6e0fb89bdca5d0193631
손은주 <sonej94113@naver.com>
2025-02-17 14:15:04 +0900

    fix:도감 전체수 추가

--- backend_project/src/main/java/com/ssafy/controller/heritagebook/HeritagebookController.java
+++ backend_project/src/main/java/com/ssafy/controller/heritagebook/HeritagebookController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.model.service.heritagebook.HeritagebookService;
 
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/api/users/{userId}/heritagebook")
 @RequiredArgsConstructor
